c730c353381c887f4c8fd816a3877ad71fffcdcb
4ppleCracker <4pplecracker@gmail.com>
2017-10-07 14:41:44 +0200

    Initial Commit

---
+++ CoolBooterCLIGUI/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CoolBooterCLIGUI
+//
+//  Created by Viggo Ingmarsson on 10/7/17.
+//  Copyright © 2017 Viggo Ingmarsson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CoolBooterCLIGUI/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CoolBooterCLIGUI
+//
+//  Created by Viggo Ingmarsson on 10/7/17.
+//  Copyright © 2017 Viggo Ingmarsson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
